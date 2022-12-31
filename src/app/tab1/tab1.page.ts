8580fc6fc5fa3a47e19370b10ddbcb280ad9edf7
Le Quyet Tien <lequyettien.it@gmail.com>
2022-12-31 15:33:23 +0700

    Add button auto search with Bing

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,48 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  keywords = [
+    "Ionic",
+    "Angular",
+    "Capacitor",
+    "Android",
+    "iOS",
+    "Android Studio",
+    "Xcode",
+    "Google Play Console",
+    "Apple Store Connect",
+    "VS Code",
+    "PHP",
+    "MySQL",
+    "HTML",
+    "CSS",
+    "Javascript",
+    "Typescript",
+    "Wordpress",
+    "Firebase",
+    "Google Cloud",
+    "Postman",
+    "Agile",
+    "Scrum",
+    "Scrum Master",
+    "Product Owner",
+    "Team member",
+    "Git",
+    "Github",
+    "Pull Request",
+    "Review",
+    "Merge"
+  ];
+  param = "&PC=EMMX01";
+
   constructor() {}
 
+  onSearchBing() {
+    this.keywords.map((keyword) => {
+      // open new tab with bing search
+      window.open('https://www.bing.com/search?q=' + keyword + this.param, '_blank');
+      // delay 1 second
+      setTimeout(() => {}, 3000);
+    });
+  }
 }
